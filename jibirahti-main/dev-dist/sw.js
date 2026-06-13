61e9c9ef79fca88f0a62263eddb5ef41c9b175a4
rayancontact09 <rayan.contact09@gmail.com>
2026-06-13 14:46:56 +0100

    Update JIBI RAHTI

--- jibirahti-main/dev-dist/sw.js
+++ jibirahti-main/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.2g8c65aotug"
+    "revision": "0.ocguogkvqf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
