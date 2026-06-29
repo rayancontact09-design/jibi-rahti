c236cacc6151f58df21c6954ad2d8116c1fe54d8
rayancontact09 <rayan.contact09@gmail.com>
2026-06-29 12:53:50 +0100

    Add multi-currency support and set light mode as default

--- jibirahti-main/dev-dist/sw.js
+++ jibirahti-main/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.c1cr633uopo"
+    "revision": "0.s8b802gg2ug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
